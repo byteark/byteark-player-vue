17741ce22a89d5681a83c597217d014aca9a740c
nonth <nonthz@gmail.com>
2022-05-30 18:12:23 +0700

    Fixed lint error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
   resolve: {
     alias: {
-      '@': require('path').resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 };
